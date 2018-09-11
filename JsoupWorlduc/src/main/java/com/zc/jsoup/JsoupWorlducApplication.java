2c73f95b5714d70df16f9c5522df44f1a87e4182
1713674660 <1713674660@qq.com>
2018-09-11 22:51:44 +0800

    Jsoup

---
+++ JsoupWorlduc/src/main/java/com/zc/jsoup/JsoupWorlducApplication.java
@@ -0,0 +1,12 @@
+package com.zc.jsoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupWorlducApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoupWorlducApplication.class, args);
+	}
+}
