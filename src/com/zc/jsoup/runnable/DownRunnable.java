a12b11d3a1ecacebd3bf99a5a205016e89821371
1713674660 <1713674660@qq.com>
2018-09-12 16:57:16 +0800

    worlduc

--- src/com/zc/jsoup/runnable/DownRunnable.java
+++ src/com/zc/jsoup/runnable/DownRunnable.java
@@ -1,11 +1,10 @@
 package com.zc.jsoup.runnable;
 
 import com.zc.jsoup.JsoupFromWorlduc;
-
 /**
- * 多线程下载
+ * 多线程
  * @author zc
- * 2018年9月12日14:09:46
+ * 2018年9月12日16:56:51
  */
 public class DownRunnable implements Runnable {
 
